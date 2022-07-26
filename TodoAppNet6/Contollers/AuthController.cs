c6966224a7a16970f1123e421c589ab677dd35c1
Amir Mohammad <sohrabi2557@gmail.com>
2022-07-26 12:25:00 +0430

    feat: create auth controller

---
+++ TodoAppNet6/Contollers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TodoAppNet6.Data;
+using TodoAppNet6.Models.Auth;
+using TodoAppNet6.Servises.Auth;
+
+namespace TodoAppNet6.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        private readonly IAuthenticationManager _authenticationManager;
+        private readonly UserManager<User> _userManager;
+
+        public AuthController(TodoContext context,
+                              IAuthenticationManager authenticationManager,
+                              UserManager<User> userManager)
+        {
+            _context = context;
+            _authenticationManager = authenticationManager;
+            _userManager = userManager;
+        }
+
+        
+    }
+}
