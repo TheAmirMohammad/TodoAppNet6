fed5e86101b7099ac00e73e240754376e232b33e
Amir Mohammad <sohrabi2557@gmail.com>
2022-07-27 17:18:20 +0430

    feat: add todo model

---
+++ TodoAppNet6/Models/TodoItem/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TodoAppNet6.Models.Auth;
+
+namespace TodoAppNet6.Models.TodoItem
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsDone { get; set; } = false;
+
+        [ForeignKey("User")]
+        public string? UserId { get; set; }
+        public virtual User? User { get; set; }
+    }
+}
---
+++ TodoAppNet6/Models/TodoItem/TodoDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAppNet6.Models.TodoItem
+{
+    public class TodoDto
+    {
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsDone { get; set; } = false;
+
+        public string? UserId { get; set; }
+    }
+}
