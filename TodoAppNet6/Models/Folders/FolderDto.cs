55d8ceb004dc98077bd6ab210d05af2a0c3257ed
Amir Mohammad <sohrabi2557@gmail.com>
2022-07-30 19:30:31 +0430

    feat: add folder model

---
+++ TodoAppNet6/Models/Folders/FolderDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TodoAppNet6.Models.TodoItem;
+
+namespace TodoAppNet6.Models.Folders
+{
+    public class FolderDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; } = string.Empty;
+
+        public string UserId { get; set; } = string.Empty;
+
+    }
+}
